b02002cc6ad588b2506f2b3955dccb23979a2f4b
SrirahmiLutfiahA <lutfiahaprilia28@gmail.com>
2021-02-16 21:48:09 +0700

    Contoh Aligment

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(      
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Button"),),
+        appBar: AppBar(title: Text("Contoh Alignment"),),
         body: Container(
-          child: RaisedButton(
-            textColor: Colors.white,
-            onPressed: () {},
-            color: Colors.red[900],
-            child:Text(
-            "Button",
-            style: TextStyle(fontSize: 20)
-            ),
-          )
+         alignment: Alignment.bottomCenter,
+         child: Text(
+           'Semangat Belajar',
+           style: TextStyle(fontSize: 20,),
+         ),
         )
       ), 
     );
