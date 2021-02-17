3038b7aabc424c8f6c3661392bba6778f6e4b071
SrirahmiLutfiahA <lutfiahaprilia28@gmail.com>
2021-02-17 19:17:46 +0700

    Contoh Margin

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(      
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Color"),),
+        appBar: AppBar(title: Text("Contoh Margin"),),
         body: Container(
-         margin: EdgeInsets.all(80),
-         height: 200,
-         width: 200,
-         alignment: Alignment.center,
-         color: Colors.amber[800],
-         child: Text(
-           'Semangat Belajar',
-           style: TextStyle(fontSize: 20, color: Colors.white),
-         )
+            margin: EdgeInsets.all(50),
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+            color: Colors.blueGrey,
+            child: Text(
+              'Semangat Belajar Flutter',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+          )
         )
       ), 
     );
