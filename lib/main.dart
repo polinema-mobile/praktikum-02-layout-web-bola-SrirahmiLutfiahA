27b2d1ab4b648bb05dfdc6f2d609c9ec6e030d78
SrirahmiLutfiahA <lutfiahaprilia28@gmail.com>
2021-02-17 22:12:58 +0700

    Contoh Gridview Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(      
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh ListView Widget"),),
-        body: ListView(
+        appBar: AppBar(title: Text("Contoh GridView Widget"),),
+        body: GridView.count(
+            crossAxisCount: 2,
             children:<Widget>[
               Container(
                 color: Colors.green,
-                alignment:Alignment.topLeft,
-                child: Text("ListView widget digunakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.", style: TextStyle(fontSize:30,color:
+                alignment:Alignment.center,
+                child: Text("1", style: TextStyle(fontSize:30,color:
                 Colors.white)),
                   ), 
 
               Container(
                 color: Colors.red,
-                alignment:Alignment.bottomCenter,
-                child: Text("ListView widget digunakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.", style: TextStyle(fontSize:30,color:
+                alignment:Alignment.center,
+                child: Text("2", style: TextStyle(fontSize:30,color:
                 Colors.white)),
                 height: 400.0,
                 width: 300.0,
@@ -32,12 +33,39 @@ class MyApp extends StatelessWidget {
               
               Container(
                 color: Colors.deepPurple,
-                alignment:Alignment.bottomCenter,
-                child: Text("ListView widget digunakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.", style: TextStyle(fontSize:30,color:
+                alignment:Alignment.center,
+                child: Text("3", style: TextStyle(fontSize:30,color:
                 Colors.white)),
                 height: 200.0,
                 width: 200.0,
                 ), 
+
+              Container(
+                color: Colors.blueGrey,
+                alignment:Alignment.center,
+                child: Text("4", style: TextStyle(fontSize:30,color:
+                Colors.white)),
+                height: 200.0,
+                width: 200.0,
+                ),
+
+              Container(
+                color: Colors.cyan[800],
+                alignment:Alignment.center,
+                child: Text("5", style: TextStyle(fontSize:30,color:
+                Colors.white)),
+                height: 200.0,
+                width: 200.0,
+                ),
+
+              Container(
+                color: Colors.yellow[800],
+                alignment:Alignment.center,
+                child: Text("6", style: TextStyle(fontSize:30,color:
+                Colors.white)),
+                height: 200.0,
+                width: 200.0,
+                ),  
             ],
           ),
         ),
