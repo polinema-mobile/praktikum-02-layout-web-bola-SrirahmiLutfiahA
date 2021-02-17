b8056497a0f81c00409336d074e954ecea4c1803
SrirahmiLutfiahA <lutfiahaprilia28@gmail.com>
2021-02-17 22:23:30 +0700

    Praktikum Ubah Title

--- lib/main.dart
+++ lib/main.dart
@@ -11,66 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(      
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh GridView Widget"),),
-        body: GridView.count(
-            crossAxisCount: 2,
-            children:<Widget>[
-              Container(
-                color: Colors.green,
-                alignment:Alignment.center,
-                child: Text("1", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                  ), 
-
-              Container(
-                color: Colors.red,
-                alignment:Alignment.center,
-                child: Text("2", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                height: 400.0,
-                width: 300.0,
-                  ), 
-              
-              Container(
-                color: Colors.deepPurple,
-                alignment:Alignment.center,
-                child: Text("3", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                height: 200.0,
-                width: 200.0,
-                ), 
-
-              Container(
-                color: Colors.blueGrey,
-                alignment:Alignment.center,
-                child: Text("4", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                height: 200.0,
-                width: 200.0,
-                ),
-
-              Container(
-                color: Colors.cyan[800],
-                alignment:Alignment.center,
-                child: Text("5", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                height: 200.0,
-                width: 200.0,
-                ),
-
-              Container(
-                color: Colors.yellow[800],
-                alignment:Alignment.center,
-                child: Text("6", style: TextStyle(fontSize:30,color:
-                Colors.white)),
-                height: 200.0,
-                width: 200.0,
-                ),  
-            ],
-          ),
-        ),
-      );
-    }
+        appBar: AppBar(
+          backgroundColor: Colors.red[800],
+          title: Text('MyApp')
+         ),
+       )
+     );
+   }
  }
 
 class MyHomePage extends StatefulWidget {
